594a98472c05ff7a4cf76802c79d307b48967411
Kyle Maclachlan <kylem831@gmail.com>
2026-04-19 22:45:49 -0800

    fix(dap): load nvim-nio before dap-ui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -221,6 +221,7 @@ return {
     "mfussenegger/nvim-dap",
     lazy = false,
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "rcarriga/nvim-dap-ui",
       "jay-babu/mason-nvim-dap.nvim",
       "theHamsta/nvim-dap-virtual-text",
